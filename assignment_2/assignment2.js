877495aaf09338bbae9a868ab3e06e545b5a1c83
Guru <guru.bagawan@gmail.com>
2017-06-08 09:38:14 -0400

    first code push

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,8 +2,11 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+for (var i = 0; i<101; i++) {
+    console.log(i);
+}
 
-
+cosole.log('loop is done')
 
 /*
 Exercise #2
@@ -17,3 +20,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var x = #;
+while (x.length>8) {
+    console.log (#)
+    x = x+#
+}
